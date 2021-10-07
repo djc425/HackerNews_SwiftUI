d52004fe594e95ba397cdc0be2740f342ab4aa37
David Chester <david.chester4@gmail.com>
2021-10-07 13:00:47 -0500

    Initial Commit

---
+++ H4X0R_News_DC/H4X0R_News_DCApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_News_DCApp.swift
+//  H4X0R_News_DC
+//
+//  Created by David Chester on 10/7/21.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_News_DCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
